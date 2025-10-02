8a65be26982511cd69df3e6472ba50494b4c5fa8
Prajwal Chavan <prajwalchavan@Prajwals-MacBook-Air.local>
2025-10-02 16:34:52 +0530

    new user end point added

--- routes/auth.js
+++ routes/auth.js
@@ -5,28 +5,44 @@ const router = Router();
 
 
 
-// Create a user using post "api/auth/" doesnt require auth
+// Create a user using post "api/auth/createUser" doesnt require auth
 
-router.post('/',[
+router.post('/createUser',[
     body('name',"Enter a valid name").isLength({min:3}),
     body('email',"Enter a valid email").isEmail(),
     body('password',"Password must be atleast 5 characters").isLength({min:5}),
-],(req,res)=>{
+],async (req,res)=>{
+    // if there are error create bad request and return the errors.
     const errors = validationResult(req);
     if(!errors.isEmpty()){
         return res.status(400).json({errors: errors.array()});
     }
     // const user = new User(req.body);
     // user.save();
-    User.create({
+// check wether email exists already.
+ try{
+     let user = await User.findOne({email: req.body.email});
+    // console.log(user);
+     if(user){
+        return res.status(400).json({error:"Sorry a user with this email already exists"});
+     }
+     user = await User.create({
         name: req.body.name,
         email: req.body.email,
         password: req.body.password
-    }).then(user=>res.json(user))
-    // res.send(req.body);
-    .catch(err=>{console.log(err);
-    res.json({error:"Please enter a unique value for email",message: err.message})});
-    //  console.log(req.body);
+    })
+    
+    // .then(user=>res.json(user))
+    // // res.send(req.body);
+    // .catch(err=>{console.log(err);
+    // res.json({error:"Please enter a unique value for email",message: err.message})});
+    //console.log(req.body);
+    res.json(user)
+}
+catch(error){
+    console.error(error.message);
+    res.status(500).send("Some error occured");
+}
 
 });
 
