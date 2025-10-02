18212a798e5be72a936ba9346198919172d802c2
Prajwal Chavan <prajwalchavan@Prajwals-MacBook-Air.local>
2025-10-02 11:29:26 +0530

    notes schema updated

--- routes/notes.js
+++ routes/notes.js
@@ -1,11 +1,29 @@
 import { Router } from 'express';
-// import Notes from '../models/notes.js';
+import Notes from '../models/Notes.js';
+import { body, validationResult } from 'express-validator';
 const router = Router();
 
 
-router.get('/',(req,res)=>{
+router.post('/',[
+     body('title',"Enter a Valid Title").isLength({min:5}),
+     body('description',"Enter a valid description").isLength({min:10}),
+     body('tag',"Enter a valid tag").isLength({min:3})
+],(req,res)=>{
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({errors: errors.array()});
+    }
 
-    res.send("Hello Notes");
+    Notes.create({
+        title:req.body.title,
+        description:req.body.description,
+        tag:req.body.tag
+    }).then(notes=>res.json(notes))
+    //  res.send(req.body);
+    .catch(err=>{console.log(err);
+    res.json({error:"Please enter a unique tag",message: err.message})});
+    console.log(req.body)
+
 })
 
 
