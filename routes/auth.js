6734e59aee24ea97f6448bf9bc76d3d091b6b85c
Prajwal Chavan <prajwalchavan@Prajwals-MacBook-Air.local>
2025-10-02 19:28:56 +0530

    login end point created for backend

--- routes/auth.js
+++ routes/auth.js
@@ -58,10 +58,53 @@ router.post('/createUser',[
 }
 catch(error){
     console.error(error.message);
-    res.status(500).send("Some error occured");
+    res.status(500).send("nternal server error occured.");
 }
 
 });
+
+
+// Authincate a user using: post "api/auth/login" no login required. //
+
+router.post('/login',[
+    body('email',"Please enter a valid email").isEmail(),
+    body("password","Password cannot be blank").exists(),
+
+],async (req,res)=>{
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({errors: errors.array()});
+    }
+  
+    const{email,password} = req.body;
+
+    try {
+        let user = await User.findOne({email:email});
+        if(!user){
+            return res.status(400).json({error:"Please try to login with correct credentials"});
+        }
+
+        const passwordCompare = await bcrypt.compare(password,user.password);
+        if(!passwordCompare){
+            return res.status(400).json({error:"Please try to login with correct credentials"});
+        }
+        const payload = {
+            user:{
+                id:user.id
+            }
+        }
+
+        const authTokenLogin = jwt.sign(payload,JWT_SECRET);
+        res.json({authTokenLogin});
+        
+    } catch (error) {
+        console.error(error.message);
+        return res.status(500).send("Internal server error occured.")
+    }
+
+});
+
+//----------------------------------------------------------------//
 
 
 
