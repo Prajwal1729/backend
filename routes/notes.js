ef190988295e25cb15e56a5f76f35ffe0730510d
Prajwal Chavan <prajwalchavan@Prajwals-MacBook-Air.local>
2025-10-04 17:15:36 +0530

    api end point updating and deleting

--- routes/notes.js
+++ routes/notes.js
@@ -39,4 +39,64 @@ try {
 
 //----------------------------------------------------------------//
 
+// update existing the notes using: PUT "/api/notes/updatenotes/:id". login required //
+
+router.put('/updatenotes/:id',fetchuser,
+    [
+        body('title',"Enter a valid title").isLength({min:3}),
+        body('description',"Description must be atleast 5 characters").isLength({min:5}),
+    ],async (req,res)=>{
+        try {
+            const{title,description,tag} = req.body;
+            const newNote = {};
+            if(title){
+                newNote.title = title;
+                newNote.description = description;
+                newNote.tag = tag;
+        }
+        // find the note to be updated and update it
+        let note = await Notes.findById(req.params.id);
+        if(!note){
+            return res.status(404).send("Not Found");
+        }
+
+        if(note.user.toString()!==req.user.id){
+            return res.status(401).send("Unauthorized access");
+        }
+
+        note = await Notes.findByIdAndUpdate(req.params.id,{$set:newNote},{new:true});
+        res.json({note});
+        } 
+        catch (error) {
+            console.error(error.message);
+            return res.status(500).send("Internal server error occured.");
+        }
+});
+
+//----------------------------------------------------------------//
+
+// delete existing the notes using: DELETE "/api/notes/deletenotes/:id". login required //
+router.delete('/deletenotes/:id',fetchuser,async(req,res)=>{
+    // const {title,description,tag} = req.body;
+    try {
+        let note = await Notes.findById(req.params.id);
+        if(!note){
+            return res.status(404).send("Not Found");
+        }
+
+        // allow deletion only if user owns this note
+        if(note.user.toString()!==req.user.id){
+            return res.status(401).send("Unauthorized access");
+        }
+        note = await Notes.findByIdAndDelete(req.params.id);
+        res.json({"Success":"Note has been deleted Successfully",note:note});
+    } catch (error) {
+        console.error(error.message);
+        return res.status(500).send("Internal server error occured.");
+    }
+});
+
+
+//---------------------------------------------------------------//
+
 export default router;
