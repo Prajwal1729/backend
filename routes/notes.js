afea1c3e29551670f0c9c3a537f2c4f5c473f907
Prajwal Chavan <prajwalchavan@Prajwals-MacBook-Air.local>
2025-10-04 10:38:55 +0530

    fetching and adding data

--- routes/notes.js
+++ routes/notes.js
@@ -1,30 +1,42 @@
 import { Router } from 'express';
 import Notes from '../models/Notes.js';
 import { body, validationResult } from 'express-validator';
+import fetchuser from '../middleware/fetchuser.js';
 const router = Router();
 
-// Route to create a note using post "api/notes/" doesnt require authentication
-router.post('/',[
-     body('title',"Enter a Valid Title").isLength({min:5}),
-     body('description',"Enter a valid description").isLength({min:10}),
-     body('tag',"Enter a valid tag").isLength({min:3})
-],(req,res)=>{
+// get all the notes using: GET "/api/notes/fetchallnotes". login required //
+router.get('/fetchallnotes',fetchuser,async (req,res)=>{
+    const notes = await Notes.find({user:req.user.id});
+    res.json(notes);
+});
+//----------------------------------------------------------------//
+
+// add new notes using: POST "/api/notes/addnewnotes". login required //
+router.post('/addnewnotes',fetchuser,[
+    body('title',"Enter a valid title").isLength({min:3}),
+    body('description',"Description must be atleast 5 characters").isLength({min:5}),
+],async (req,res)=>{ 
+try {
+    const {title,description,tag} = req.body;
     const errors = validationResult(req);
     if(!errors.isEmpty()){
         return res.status(400).json({errors: errors.array()});
     }
+    const note = new Notes({
+        title,
+        description,
+        tag,
+        user: req.user.id
+    })
 
-    Notes.create({
-        title:req.body.title,
-        description:req.body.description,
-        tag:req.body.tag
-    }).then(notes=>res.json(notes))
-    //  res.send(req.body);
-    .catch(err=>{console.log(err);
-    res.json({error:"Please enter a unique tag",message: err.message})});
-    // console.log(req.body)
+    const saveNote = await note.save();
+    res.json(saveNote);
+} catch (error) {
+    console.error(error.message);
+    return res.status(500).send("Internal server error occured.");
+}
+});
 
-})
-
+//----------------------------------------------------------------//
 
 export default router;
